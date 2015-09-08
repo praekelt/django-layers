3bc6b81e2ae2093b5572a2bc8552ccb139f98c30
hedleyroos <hedleyroos@gmail.com>
2015-09-08 12:46:51 +0200

    Rewrite decorator to be function based because it makes it easier to use in urls.py

--- layers/decorators.py
+++ layers/decorators.py
@@ -1,29 +1,33 @@
+from functools import wraps
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.decorators import available_attrs
 from django.conf import settings
 
 
-class exclude_from_layers:
+def exclude_from_layers(layers):
 
-    def __init__(self, layers=[]):
-        self.layers = layers
-
-    def __call__(self, func):
-
-        def new(cls, request, *args, **kwargs):
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
             try:
                 layer = settings.LAYERS["layers"][-1]
             except (AttributeError, KeyError):
                 layer = None
 
             # Redirect if view not available in this layer
-            if layer and (layer in self.layers):
+            if layer and (layer in layers):
                 return render_to_response(
                     "layers/exclude_from_layers.html",
                     {"layer": layer},
-                    context_instance=RequestContext(request)
+                    context_instance=RequestContext(
+                        getattr(request, "request", request)
+                    )
                 )
+                return response
 
-            return func(cls, request, *args, **kwargs)
+            return view_func(request, *args, **kwargs)
 
-        return new
+        return _wrapped_view
+    return decorator
