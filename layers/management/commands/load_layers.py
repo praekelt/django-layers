3ef1886cd6b4e5b6d8156221ef7862ac77e4e741
hedleyroos <hedleyroos@gmail.com>
2016-12-19 14:49:58 +0200

    Create an accessor for LAYER_STACKS

--- layers/management/commands/load_layers.py
+++ layers/management/commands/load_layers.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from layers import LAYER_STACKS
+from layers import get_layer_stacks, reset_layer_stacks, build_layer_stacks
 from layers.models import Layer
 
 
@@ -10,6 +10,8 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        for layers in LAYER_STACKS.values():
+        reset_layer_stacks()
+        build_layer_stacks()
+        for layers in get_layer_stacks().values():
             for layer in layers:
                 Layer.objects.get_or_create(name=layer)
